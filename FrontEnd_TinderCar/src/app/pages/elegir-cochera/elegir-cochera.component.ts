96fd98015272c481b7a450e680c0c6bd4cf398d3
Valentina Tommasini <valetommasini94@gmail.com>
2023-05-07 12:11:35 -0300

    agrego componente elegirCochera

--- FrontEnd_TinderCar/src/app/pages/elegir-cochera/elegir-cochera.component.ts
+++ FrontEnd_TinderCar/src/app/pages/elegir-cochera/elegir-cochera.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-elegir-cochera',
   templateUrl: './elegir-cochera.component.html',
   styleUrls: ['./elegir-cochera.component.css']
 })
-export class ElegirCocheraComponent {
-
-}
+export class ElegirCocheraComponent {}
